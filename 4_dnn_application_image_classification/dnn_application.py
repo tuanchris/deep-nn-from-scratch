bfa10b628ba55f57670d731b600efbca03c2253f
Tuan Nguyen <tuannguyen@Tuans-MacBook-Pro.local>
2019-12-04 11:19:58 +0700

    feat: add dnn application: image classification implementation

---
+++ 4_dnn_application_image_classification/dnn_application.py
@@ -0,0 +1,95 @@
+import time
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+from dnn_app_utils_v3 import *
+from deep_neural_network import two_layer_model, L_layer_model
+
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+%load_ext autoreload
+%autoreload 2
+
+np.random.seed(1)
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+# Example of a picture
+index = 10
+plt.imshow(train_x_orig[index])
+print ("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
+
+# Explore our dataset
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+
+print ("Number of training examples: " + str(m_train))
+print ("Number of testing examples: " + str(m_test))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_x_orig shape: " + str(train_x_orig.shape))
+print ("train_y shape: " + str(train_y.shape))
+print ("test_x_orig shape: " + str(test_x_orig.shape))
+print ("test_y shape: " + str(test_y.shape))
+
+# Reshape the training and test examples
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+# Standardize data to have feature values between 0 and 1.
+train_x = train_x_flatten/255.
+test_x = test_x_flatten/255.
+
+print ("train_x's shape: " + str(train_x.shape))
+print ("test_x's shape: " + str(test_x.shape))
+
+### CONSTANTS DEFINING THE MODEL ####
+n_x = 12288     # num_px * num_px * 3
+n_h = 7
+n_y = 1
+layers_dims = (n_x, n_h, n_y)
+
+parameters = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost=True)
+predictions_train = predict(train_x, train_y, parameters)
+predictions_test = predict(test_x, test_y, parameters)
+
+'''
+    We can notice that running the model on fewer iterations (say 1500) gives
+    better accuracy on the test set. Early stopping is a way to prevent overfitting.
+
+    It seems that our 2-layer neural network has better performance (72%) than
+    the logistic regression implementation (70%). Let's see if we can do even
+    better with an LL -layer model.
+'''
+
+### CONSTANTS ###
+layers_dims = [12288, 20, 7, 5, 1] #  4-layer model
+
+parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
+
+pred_train = predict(train_x, train_y, parameters)
+pred_test = predict(test_x, test_y, parameters)
+
+'''
+It seems that our 4-layer neural network has better performance (80%) than our
+2-layer neural network (72%) on the same test set.
+'''
+
+print_mislabeled_images(classes, test_x, test_y, pred_test)
+
+'''
+    A few types of images the model tends to do poorly on include:
+
+        - Cat body in an unusual position
+        - Cat appears against a background of a similar color
+        - Unusual cat color and species
+        - Camera Angle
+        - Brightness of the picture
+        - Scale variation (cat is very large or small in image)
+'''
